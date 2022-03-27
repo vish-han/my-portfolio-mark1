57674ffb994cca13b008149e735f6552c984dfe5
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-03-27 22:49:14 +0530

    pic responsiveness corrected'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import About from "./Components/About";
 import Intro from "./Components/Intro";
 
 function App() {
   return (
     <>
       <Intro />
+      <About />
     </>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../Components/About.css";
+
+const About = () => {
+  return <div className="a">About</div>;
+};
+
+export default About;
