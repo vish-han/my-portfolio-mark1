576e3d1aa94a5664911026f3ace03ff3bea2b484
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-03-30 00:12:41 +0530

    bg changed

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,16 @@ import React from "react";
 import "../Components/About.css";
 
 const About = () => {
-  return <div className="a">About</div>;
+  return (
+    <div className="a">
+      <div className="a-left"></div>
+      <div className="a-right">
+        <div className="skills">
+          <h2>Web Development</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
